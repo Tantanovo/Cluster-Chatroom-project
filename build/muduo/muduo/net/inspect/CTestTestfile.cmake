490808049730c3cc87786f3c07d752da44b62ac3
Tantanovo <1348215719@qq.com>
2026-06-24 20:45:40 +0800

    chore: 添加 .gitignore，移除编译产物/第三方源码/运行时数据的跟踪

--- build/muduo/muduo/net/inspect/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yzy/Cluster-Chatroom/Cluster-Chatroom-project/muduo/muduo/net/inspect
-# Build directory: /home/yzy/Cluster-Chatroom/Cluster-Chatroom-project/build/muduo/muduo/net/inspect
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/muduo/muduo/net/protobuf/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yzy/Cluster-Chatroom/Cluster-Chatroom-project/muduo/muduo/net/protobuf
-# Build directory: /home/yzy/Cluster-Chatroom/Cluster-Chatroom-project/build/muduo/muduo/net/protobuf
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
